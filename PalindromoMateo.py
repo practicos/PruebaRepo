20f87571710f8e0d4011280051953f630e171b33
Mateo Arbelaez <mateoarbelaez@seti.com.co>
2019-05-21 21:33:35 -0500

    Se agrega palindromo

---
+++ PalindromoMateo.py
@@ -0,0 +1,10 @@
+# Programa para comparar una cadena para saber si es palindroma.
+#1.Se captura la palabra a evaluar.
+Captura=input("Ingrese palabra a evaluar:")
+#2.Se Invierte la palabra 
+Torcido= "".join(reversed(Captura))
+#Se realiza la condicion se quitan espacios si existen para asegurar la palabra 
+if Captura.replace(" ","")==Torcido.replace(" ",""):
+    print("La palabra "+ Captura + " Es palindroma")
+else: 
+    print("La palabra "+ Captura + " No es palindroma")
